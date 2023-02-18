d967e33101419466bd021cbaf16ddbdca167e293
Dmitry G <sdprecovery@gmail.com>
2023-02-18 12:59:08 +0300

    Frontend working loginUser useState

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -10,9 +10,14 @@ export default AuthContext;
 
 
 export const AuthProvider = ({ children }) => {
-    // localStorage.getItem('authTokens')
-    let [authTokens, setAuthTokens] = useState(null);
-    let [user, setUser] = useState(null);
+    let [authTokens, setAuthTokens] = useState(
+        localStorage.getItem('authTokens')
+            ? JSON.parse(localStorage.getItem('authTokens'))
+            : null);
+    let [user, setUser] = useState(
+        localStorage.getItem('authTokens')
+            ? jwt_decode(localStorage.getItem('authTokens'))
+            : null);
     const navigate = useNavigate();
 
     let loginUser = async (e) => {
