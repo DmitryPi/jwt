9bdec6150e12867b5102a8262387eb9d082983ad
Dmitry G <sdprecovery@gmail.com>
2023-02-18 15:52:59 +0300

    Front token update on expire

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -59,7 +59,7 @@ export const AuthProvider = ({ children }) => {
                 'Content-Type': "application/json"
             },
             body: JSON.stringify(
-                { 'refresh': authTokens.refresh })
+                { 'refresh': authTokens?.refresh })
         });
         let data = await response.json();
         if (response.status === 200) {
@@ -68,6 +68,10 @@ export const AuthProvider = ({ children }) => {
             localStorage.setItem('authTokens', JSON.stringify(data));
         } else {
             logoutUser();
+        }
+
+        if (loading) {
+            setLoading(false);
         }
     };
 
@@ -79,6 +83,11 @@ export const AuthProvider = ({ children }) => {
     };
 
     useEffect(() => {
+
+        if (loading) {
+            updateToken();
+        }
+
         let fourMinutes = 1000 * 60 * 4;
         let interval = setInterval(() => {
             if (authTokens) {
@@ -91,7 +100,7 @@ export const AuthProvider = ({ children }) => {
 
     return (
         <AuthContext.Provider value={contextData}>
-            {children}
+            {loading ? null : children}
         </AuthContext.Provider>
     );
 };
