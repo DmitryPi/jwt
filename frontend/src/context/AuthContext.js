1dfa366b6d0d9c88ae40b7a9d5aa8e2763a7b739
Dmitry G <sdprecovery@gmail.com>
2023-02-18 14:28:41 +0300

    Front auth token user persistance

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import jwt_decode from 'jwt-decode';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -18,6 +18,7 @@ export const AuthProvider = ({ children }) => {
         localStorage.getItem('authTokens')
             ? jwt_decode(localStorage.getItem('authTokens'))
             : null);
+    let [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     let loginUser = async (e) => {
@@ -50,11 +51,41 @@ export const AuthProvider = ({ children }) => {
         navigate('/login');
     };
 
+    let updateToken = async () => {
+        console.log('Token called');
+        let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(
+                { 'refresh': authTokens.refresh })
+        });
+        let data = await response.json();
+        if (response.status === 200) {
+            setAuthTokens(data);
+            setUser(jwt_decode(data.access));
+            localStorage.setItem('authTokens', JSON.stringify(data));
+        } else {
+            logoutUser();
+        }
+    };
+
     let contextData = {
         user: user,
         loginUser: loginUser,
         logoutUser: logoutUser,
     };
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            if (authTokens) {
+                updateToken();
+            }
+        }, 2000);
+        return () => clearInterval(interval);
+
+    }, [authTokens, loading]);
 
     return (
         <AuthContext.Provider value={contextData}>
